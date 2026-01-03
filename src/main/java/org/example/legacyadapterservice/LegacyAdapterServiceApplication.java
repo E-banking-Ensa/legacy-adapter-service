6bee7961a70459ba0154f0626d36dd20358e47fe
fahlaoui <fahlaouimohammed@gmail.com>
2026-01-03 17:25:27 +0100

    eurika

--- src/main/java/org/example/legacyadapterservice/LegacyAdapterServiceApplication.java
+++ src/main/java/org/example/legacyadapterservice/LegacyAdapterServiceApplication.java
@@ -2,8 +2,10 @@ package org.example.legacyadapterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LegacyAdapterServiceApplication {
 
     public static void main(String[] args) {
